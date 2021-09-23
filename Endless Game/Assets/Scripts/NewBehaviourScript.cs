e10c3b0106a6e0f72f231c0c5e25bae89b517a75
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-09-23 21:06:27 +0700

    Update Character, Ground

---
+++ Endless Game/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+}
