6f75ff313c7a4d651fa9061c9c4d66fced02281c
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-09-24 15:46:26 +0700

    Make Tile

--- Endless Game/Assets/Scripts/NewBehaviourScript.cs
+++ Endless Game/Assets/Scripts/NewBehaviourScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,12 @@ public class NewBehaviourScript : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed;
+    private int desiredLane = 1; //0: left, 1: middle, 2: right
+    public float laneDistance = 4; // distance between two lanes
+
+    public float jumpForce;
+    public float Gravity = -20;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,7 +24,61 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         direction.z = forwardSpeed;
+
+        if (controller.isGrounded)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            direction.y += Gravity * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3) desiredLane = 2;
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1) desiredLane = 0;
+
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        
+        if (desiredLane == 0)
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }else if(desiredLane == 2)
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        //transform.position = targetPosition;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 70 * Time.deltaTime);
+        controller.center = controller.center;
+        //if (transform.position == targetPosition)
+        //    return;
+        //Vector3 diff = targetPosition - transform.position;
+        //Vector3 moveDir = diff.normalized * 25 * 25 * Time.deltaTime;
+        //if (moveDir.sqrMagnitude < diff.sqrMagnitude)
+        //    controller.Move(moveDir);
+        //else
+        //    controller.Move(diff);
     }
+
+    private void Jump()
+    {
+        direction.y = jumpForce;
+    }
+
     private void FixedUpdate()
     {
         controller.Move(direction * Time.fixedDeltaTime);
---
+++ Endless Game/Assets/Scripts/TileManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    public float tileLength = 30;
+    public int numberOfTiles = 5;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<numberOfTiles; i++)
+        {
+            if (i == 0)
+                SpawnTile(0);
+            else
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.z - 45 > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+
+        GameObject go =  Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(go);
+        zSpawn += tileLength;
+
+    }
+}
